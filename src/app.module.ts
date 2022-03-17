b54a5f6cd19545faeccd709216836d25f26514b7
유한결 <yuhangyeol@yuhangyeols-MacBook-Air.local>
2022-03-17 14:23:02 +0900

    hangyeol0317

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'flog-project',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'mysql',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'scr/common/graphql/schema.gql',
+      context: ({ req, res }) => ({ req, res }),
+    }),
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}

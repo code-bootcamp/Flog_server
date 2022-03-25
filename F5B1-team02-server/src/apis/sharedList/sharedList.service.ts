fec999e154f36e2cb677cdc32b8d111b58889cdc
Jiwon <jiwon8518@naver.com>
2022-03-25 14:21:07 +0900

    Modify: [#41] 여행족보 조회-주석 삭제

--- F5B1-team02-server/src/apis/sharedList/sharedList.service.ts
+++ F5B1-team02-server/src/apis/sharedList/sharedList.service.ts
@@ -5,10 +5,6 @@ import { Board } from '../board/entities/board.entity';
 import { DetailSchedule } from '../detailSchedule/entities/detailSchedule.entity';
 import { Schedule } from '../schedule/entities/schedule.entity';
 
-interface IFindOne {
-  scheduleId: string;
-}
-
 @Injectable()
 export class ShareScheduleService {
   constructor(
@@ -31,28 +27,6 @@ export class ShareScheduleService {
     return myQts;
   }
 
-  //   async findOne({ scheduleId }: IFindOne) {
-  //     return await this.scheduleRepository.findOne({
-  //       where: { id: scheduleId },
-  //     });
-  //   }
-
-  // async findMyQt1({ scheduleId }) {
-  //   const shareSchedule = await this.scheduleRepository
-  //     .createQueryBuilder('board')
-  //     .innerJoinAndSelect('board.schedule', 'schedule')
-  //     //   .select('board')
-  //     .from(Board, 'board')
-  //     .addFrom(Schedule, 'schedule')
-  //     //   .innerJoin('board.scheduleId', 'schedule')
-  //     .where('board.scheduleId = :scheduleId', { scheduleId: scheduleId })
-  //     .orderBy({
-  //       'board.day': 'ASC',
-  //     })
-  //     .getMany();
-
-  //   return shareSchedule;
-  // }
   async findMyQt1({ scheduleId }) {
     const shareSchedule = await this.boardRepository
       .createQueryBuilder()

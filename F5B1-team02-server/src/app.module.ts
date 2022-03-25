4845a740896eb6534b2be010db04775001ba31ce
Jiwon <jiwon8518@naver.com>
2022-03-25 16:54:03 +0900

    Modify :[#46] 일반회원 로그아웃

--- F5B1-team02-server/src/app.module.ts
+++ F5B1-team02-server/src/app.module.ts
@@ -47,7 +47,7 @@ import { ShareScheduleModule } from './apis/sharedList/sharedList.module';
       host: 'my_database',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: '1234',
       database: 'flog',
       entities: [__dirname + '/apis/**/*.entity.*'],
       synchronize: true,

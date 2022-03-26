151fa75f7e1b88bb3ce83b746bc55145480cbeec
Jiwon <jiwon8518@naver.com>
2022-03-26 19:21:39 +0900

    Modify: [#52] 배너이미지 UseGuards 추가

--- F5B1-team02-server/src/apis/bannerImage/dto/updateBannerImage.input.ts
+++ F5B1-team02-server/src/apis/bannerImage/dto/updateBannerImage.input.ts
@@ -1,5 +1,4 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { HASHTAG, NUMBER_PEOPLE_ENUM } from '../entities/schedule.entity';
 
 @InputType()
 export class updateBannerImageInput {

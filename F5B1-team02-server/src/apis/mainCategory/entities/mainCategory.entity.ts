a36b67636c7617b7c55d62b12ed7beea6c930b80
유한결 <yuhangyeol@yuhangyeols-MacBook-Air.local>
2022-03-27 13:43:46 +0900

    Modify:[#38] 메인카테고리 수정

--- F5B1-team02-server/src/apis/mainCategory/entities/mainCategory.entity.ts
+++ F5B1-team02-server/src/apis/mainCategory/entities/mainCategory.entity.ts
@@ -1,5 +1,14 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum MAINCATEGORY_ENUM {
+  MINE = '나의 여행',
+  OUR = '우리의 여행',
+  MYPAGE = '마이페이지',
+}
+registerEnumType(MAINCATEGORY_ENUM, {
+  name: 'MAINCATEGORY_ENUM',
+});
 
 @Entity()
 @ObjectType()
@@ -8,7 +17,7 @@ export class MainCategory {
   @Field(() => String)
   id: string;
 
-  @Column()
-  @Field(() => String)
-  name: string;
+  @Column({ type: 'enum', enum: MAINCATEGORY_ENUM })
+  @Field(() => MAINCATEGORY_ENUM)
+  name: MAINCATEGORY_ENUM;
 }
